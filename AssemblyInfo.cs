2ee5243938be2abf229021f08bc5ec7763626cb2
jsmorley <jsmorley@gmail.com>
2019-01-30 22:44:47 -0500

    First Update.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCopyright("© 2019 - Jeffrey Morley")]
+[assembly: AssemblyVersion("2.3.0.0")]
+
+// Do not change the entries below!
+#if X64
+[assembly: AssemblyInformationalVersion("4.2.0.3111 (64-bit)")]
+#else
+[assembly: AssemblyInformationalVersion("4.2.0.3111 (32-bit)")]
+#endif
+[assembly: AssemblyProduct("Rainmeter")]
